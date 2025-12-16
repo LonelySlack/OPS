8de6437c8213ecdf6cd6552e5b51385e337d3797
LonelySlack <aqil28042003@yahoo.com>
2025-12-16 22:50:26 +0800

    reformat

--- rentverse-backend-main/src/modules/bookings/index.js
+++
@@ -1,9 +0,0 @@
-const bookingsController = require('./bookings.controller');
-const bookingsService = require('./bookings.service');
-const bookingsRoutes = require('./bookings.routes');
-
-module.exports = {
-  controller: bookingsController,
-  service: bookingsService,
-  routes: bookingsRoutes,
-};
